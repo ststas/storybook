548acd13025c1a4622dbe6684aa8a490653c27bd
Stas Zaitsev <ststas@yandex.ru>
2025-06-01 13:06:39 +0300

    feat: update files for deploy

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,14 +1,21 @@
 import type { StorybookConfig } from '@storybook/react-vite';
+import { mergeConfig } from 'vite';
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  stories: [
+    '../src/**/*.mdx',
+    '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)',
   ],
-  "addons": [],
-  "framework": {
-    "name": "@storybook/react-vite",
-    "options": {}
-  }
+  addons: [],
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
+  },
+  viteFinal: async (config) => {
+    return mergeConfig(config, {
+      base: '/storybook/',
+    });
+  },
 };
+
 export default config;
